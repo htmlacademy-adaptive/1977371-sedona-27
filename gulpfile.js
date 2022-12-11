e4659bab80f288bf809f91fd258f4748bbbede6a
Keks <keks@htmlacademy.ru>
2022-12-11 13:48:22 +0300

    :cat:

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
+<<<<<<< HEAD
 import sass from 'gulp-dart-sass';
+=======
+import less from 'gulp-less';
+>>>>>>> 96c1f7f (:cat:)
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
@@ -8,9 +12,15 @@ import browser from 'browser-sync';
 // Styles
 
 export const styles = () => {
+<<<<<<< HEAD
   return gulp.src('source/sass/style.scss', { sourcemaps: true })
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
+=======
+  return gulp.src('source/less/style.less', { sourcemaps: true })
+    .pipe(plumber())
+    .pipe(less())
+>>>>>>> 96c1f7f (:cat:)
     .pipe(postcss([
       autoprefixer()
     ]))
@@ -35,7 +45,11 @@ const server = (done) => {
 // Watcher
 
 const watcher = () => {
+<<<<<<< HEAD
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+=======
+  gulp.watch('source/less/**/*.less', gulp.series(styles));
+>>>>>>> 96c1f7f (:cat:)
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
