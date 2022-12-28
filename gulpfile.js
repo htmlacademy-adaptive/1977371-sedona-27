ab5e9f481334670d6968a1df95f81decf790b46e
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2022-12-28 22:15:13 +0600

    scss на less

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,6 @@
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
-
-import sass from 'gulp-dart-sass';
-
 import less from 'gulp-less';
-
-import sass from 'gulp-dart-sass';
-
-import less from 'gulp-less';
-
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
@@ -16,22 +8,9 @@ import browser from 'browser-sync';
 // Styles
 
 export const styles = () => {
-
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-
-  return gulp.src('source/less/style.less', { sourcemaps: true })
-    .pipe(plumber())
-
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-
   return gulp.src('source/less/style.less', { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
-
     .pipe(postcss([
       autoprefixer()
     ]))
@@ -56,8 +35,6 @@ const server = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/less/**/*.less', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
