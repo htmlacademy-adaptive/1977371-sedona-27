b6ebdffb0c2a5e11d371088fe7eef981ea4fc810
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2023-01-19 22:07:10 +0300

    22:07

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ const createWebp = () => {
   // SVG
 
 const svg = () =>
-gulp.src(['source/img/icons/*.svg', 'source/icons/*.svg'])
+gulp.src(['source/img/icons/*.svg', 'source/img/icons/*.svg'])
 .pipe(svgo())
 .pipe(gulp.dest('build/img'));
 
