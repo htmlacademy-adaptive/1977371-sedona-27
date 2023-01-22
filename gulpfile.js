ca0bb03baa558cf8b91de71ad64ffcf79e6b7d75
Евгений Калинин <97776034+EvgeniyDobrov@users.noreply.github.com>
2023-01-22 09:54:22 +0300

    good

--- gulpfile.js
+++ gulpfile.js
@@ -218,10 +218,6 @@ import svgo from 'gulp-svgmin';
 import { stacksvg } from 'gulp-stacksvg';
 import {deleteAsync} from 'del';
 import browser from 'browser-sync';
-import squoosh from 'gulp-libsquoosh';
-import svgo from 'gulp-svgmin';
-import svgstore from 'gulp-svgstore';
-import del from 'del';
 
 // Styles
 
